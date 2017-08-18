abc727df9c0606be50d02f7a15762195e2b5075e
Hamza Khan <coolbuddy_96@hotmail.com>
2017-08-18 16:48:13 -0700

    first commit

---
+++ server/main.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import {WebApp} from 'meteor/webapp';
+
+import '../imports/api/users';
+import '../imports/startup/simpl-schema-configuration.js';
+
+
+//req = HTTP request
+// res= HTTP response
+// next keeps the app moving
+Meteor.startup(() => {
+
+});
